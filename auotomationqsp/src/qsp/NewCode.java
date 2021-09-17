bbaf53cab92208538d0afd08f75a8195778e2cdd
leenaingale99 <90598114+leenaingale99@users.noreply.github.com>
2021-09-17 09:17:39 +0530

    Update NewCode.java

--- auotomationqsp/src/qsp/NewCode.java
+++ auotomationqsp/src/qsp/NewCode.java
@@ -4,7 +4,7 @@ public class NewCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hiiiiii byeee");
+		System.out.println("Leena Ingale");
 	}
 
 }
